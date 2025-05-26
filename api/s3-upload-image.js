23be8041147c80dcdd3967add491a191c32266fe
Anıl <anilnuhoglu@gmail.com>
2025-05-26 22:08:33 +0300

    admin

--- api/s3-upload-image.js
+++ api/s3-upload-image.js
@@ -11,27 +11,36 @@ const s3 = new S3Client({
 })
 
 export default async function handler(req, res) {
-  if (req.method !== 'GET') {
-    return res.status(405).json({ error: "Method not allowed" })
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS")
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type")
+  
+    if (req.method === "OPTIONS") {
+      return res.status(200).end()
+    }
+  
+    if (req.method !== 'GET') {
+      return res.status(405).json({ error: "Method not allowed" })
+    }
+  
+    try {
+      const ext = req.query.ext || "jpg"
+      const filename = `images/${uuidv4()}.${ext}`
+  
+      const command = new PutObjectCommand({
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: filename,
+        ContentType: `image/${ext}`
+      })
+  
+      const url = await getSignedUrl(s3, command, { expiresIn: 300 })
+  
+      const publicUrl =`https://pirlo-menu-app.s3.eu-central-1.amazonaws.com/${filename}`
+  
+      res.status(200).json({ url, publicUrl })
+    } catch (err) {
+      console.error(err)
+      res.status(500).json({ error: "Failed to generate image upload URL" })
+    }
   }
-
-  try {
-    const ext = req.query.ext || "jpg"
-    const filename = `images/${uuidv4()}.${ext}`
-
-    const command = new PutObjectCommand({
-      Bucket: process.env.AWS_BUCKET_NAME,
-      Key: filename,
-      ContentType: `image/${ext}`
-    })
-
-    const url = await getSignedUrl(s3, command, { expiresIn: 300 })
-
-    const publicUrl = `https://pirlo-menu-app.s3.eu-central-1.amazonaws.com/images`
-
-    res.status(200).json({ url, publicUrl })
-  } catch (err) {
-    console.error(err)
-    res.status(500).json({ error: "Failed to generate image upload URL" })
-  }
-}
+  
